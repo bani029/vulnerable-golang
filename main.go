5fa2eeb7928d0362554a71616c8ed85b0e64d2b7
bani029 <spanigrahi@endor.ai>
2026-09-03 10:09:12 +0530

    I hid these 	//_ "github.com/gogo/protobuf/proto" 	//_ "github.com/hashicorp/golang-lru" 	//_ "github.com/owncast/owncast/logging"

--- main.go
+++ main.go
@@ -5,10 +5,10 @@ import (
 	"os"
 	"os/exec"
 
-	//_ "github.com/dgrijalva/jwt-go"
+	_ "github.com/dgrijalva/jwt-go"
 	//_ "github.com/gogo/protobuf/proto"
 	//_ "github.com/hashicorp/golang-lru"
-	_ "github.com/owncast/owncast/logging"
+	//_ "github.com/owncast/owncast/logging"
 )
 
 func main() {
